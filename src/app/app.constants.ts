9614eebe3c4d285231570e32902270d63264bec5
Luiz cavalieri <luiz.cavalieri@gmail.com>
2017-03-11 17:22:43 +1100

    Adding Md5 hash generator and changing API Keys and Public Keys.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -17,6 +17,6 @@
 
 
 export const MARVEL_API_URL = 'http://gateway.marvel.com:80/v1/public/';
-export const PRIVATE_KEY = 'cd4ff2b15d809de5f84e369e03a7d399c52e4772';
-export const PUBLIC_KEY = 'd128928f89eab7dca3a7f3c003c48f80';
+export const PRIVATE_KEY = 'b1ffa2511e30bad2aabdd61afc248ae30d97fd6f';
+export const PUBLIC_KEY = '135e48a7e09ef621e8806df1d2935ab0';
 export const TS = '1';
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,24 +1,47 @@
 import { Injectable } from '@angular/core';
-import { MARVEL_API_URL } from '../app.constants';
+import { MARVEL_API_URL, PUBLIC_KEY, TS, PRIVATE_KEY } from '../app.constants';
 
 import { Http, Response } from '@angular/http';
 import { HeroModel } from '../model/hero.model';
 import { Observable } from 'rxjs';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Injectable()
 export class HeroService {
 
   heroes: HeroModel [];
 
-  constructor(private http: Http) {
+  md5 = new Md5();
 
-    this.printHeroes();
+  hashString: String;
+
+  constructor(private http: Http) { }
+
+
+
+  getHeroes(): Observable<HeroModel[]> {
+
+
+    this.md5.appendStr(TS).appendStr(PRIVATE_KEY).appendStr(PUBLIC_KEY);
+
+    this.hashString = this.md5.end().toString();
+
+    console.log(this.hashString);
+
+    return this.http.get(`${MARVEL_API_URL}/characters?apikey=${PUBLIC_KEY}&ts=${TS}&hash=${this.hashString}`)
+      .map(response => response.json().data.results
+      );
 
   }
 
 
   printHeroes(){
-    const response$ = this.http.get(`${MARVEL_API_URL}/characters?apikey=d128928f89eab7dca3a7f3c003c48f80&ts=1&hash=c8d6f1e0ec832edafe8c4242ac8cc9e8`)
+
+    this.md5.appendStr(TS).appendStr(PRIVATE_KEY).appendStr(PUBLIC_KEY);
+
+    this.hashString = this.md5.end().toString();
+
+    const response$ = this.http.get(`${MARVEL_API_URL}/characters?apikey=${PUBLIC_KEY}&ts=${TS}&hash=${this.hashString}`)
       .map((res:Response) => res.json());
 
     response$.subscribe(
@@ -28,23 +51,4 @@ export class HeroService {
     );
   }
 
-  getHeroes(): Observable<HeroModel[]> {
-    return this.http.get(`${MARVEL_API_URL}/characters?apikey=d128928f89eab7dca3a7f3c003c48f80&ts=1&hash=c8d6f1e0ec832edafe8c4242ac8cc9e8`)
-      .map(response => response.json().data.results
-      );
-
-
-  }
-
-
-
-
-  // getHeroes() {
-  //   // return this.http.get(`${MARVEL_API_URL}/characters?apikey=d128928f89eab7dca3a7f3c003c48f80&ts=1&hash=c8d6f1e0ec832edafe8c4242ac8cc9e8`)
-  //   //   .map(res => res.json())
-  //   //   .subscribe(
-  //   //     results => this.heroes = results
-  //   //   );
-  //
-  // }
 }
